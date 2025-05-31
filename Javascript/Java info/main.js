1a6b4df3d12d8a7852aa9a368b69d0181025f445
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-05-31 13:00:37 -0300

    Actualizar main.js

--- Javascript/Java info/main.js
+++ Javascript/Java info/main.js
@@ -1,1 +1,24 @@
 
+//alert("soy javascript");
+
+/* 2.
+Declara dos variables: admin y name.
+Asigna el valor "John" a name.
+Copia el valor de name a admin.
+Muestra el valor de admin usando alert (debe salir “John”).
+let admin, name;
+name= 'John';
+admin= name;
+alert(admin);
+*/
+
+/* 3.
+ Crea una variable con el nombre de nuestro planeta. ¿Cómo nombrarías a dicha variable?
+Crea una variable para almacenar el nombre del usuario actual de un sitio web. ¿Cómo nombrarías a dicha variable?
+let  ourPlanetName = "tierra";
+let userName= "pascal";
+*/
+
+
+
+
