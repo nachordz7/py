32330b2e00a6dac712c12785f516332405acfa49
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-06-30 17:34:24 -0300

    Update main.js

--- Javascript/Java info/main.js
+++ Javascript/Java info/main.js
@@ -1,24 +1,36 @@
+// FUNDAMENTOS EN JAVASCRIPT
 
-//alert("soy javascript");
+// 1. ¡Hola, mundo!
+/*
+Crea una página que muestre el mensaje “¡Soy JavaScript!”.
+alert("¡Soy JavaScript!");
+*/
 
-/* 2.
+// 2. Estructura del codigo
+// 3. El modo moderno, "use strict"
+/*
+(no hay tarea).
+*/
+
+// 4. Variables
+/*
 Declara dos variables: admin y name.
 Asigna el valor "John" a name.
 Copia el valor de name a admin.
 Muestra el valor de admin usando alert (debe salir “John”).
-let admin, name;
-name= 'John';
-admin= name;
+let admin;
+let name = "John";
+admin = name;
 alert(admin);
-*/
 
-/* 3.
- Crea una variable con el nombre de nuestro planeta. ¿Cómo nombrarías a dicha variable?
+Crea una variable con el nombre de nuestro planeta. ¿Cómo nombrarías a dicha variable?
 Crea una variable para almacenar el nombre del usuario actual de un sitio web. ¿Cómo nombrarías a dicha variable?
-let  ourPlanetName = "tierra";
-let userName= "pascal";
+
+let ourPlanetName = "Tierra";
+let currentUserName = "Juan";
 */
 
+// 5. Tipos de datos
+/*
 
-
-
+*/
