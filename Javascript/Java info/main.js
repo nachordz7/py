780425d6f8c38d1c7514d021cad636e5e6632ea3
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-07-06 15:16:01 -0300

    Update main.js

--- Javascript/Java info/main.js
+++ Javascript/Java info/main.js
@@ -1,59 +1,27 @@
-// FUNDAMENTOS EN JAVASCRIPT
+//Boton que agrega un elemento li dentro de ul
+let btn = document.querySelector('#btn'); // Obtiene el elemento boton con id btn
+let ul = document.querySelector('ul');    // Obtiene el elemento ul por default
 
-// 1. ¡Hola, mundo!
-/*
-Crea una página que muestre el mensaje “¡Soy JavaScript!”.
-alert("¡Soy JavaScript!");
-*/
-
-// 2. Estructura del codigo
-// 3. El modo moderno, "use strict"
-/*
-(no hay tarea).
-*/
-
-// 4. Variables
-/*
-Declara dos variables: admin y name.
-Asigna el valor "John" a name.
-Copia el valor de name a admin.
-Muestra el valor de admin usando alert (debe salir “John”).
-let admin;
-let name = "John";
-admin = name;
-alert(admin);
-
-Crea una variable con el nombre de nuestro planeta. ¿Cómo nombrarías a dicha variable?
-Crea una variable para almacenar el nombre del usuario actual de un sitio web. ¿Cómo nombrarías a dicha variable?
-
-let ourPlanetName = "Tierra";
-let currentUserName = "Juan";
-*/
-
-// 10. Ejecución condicional: if, '?'
-/*
-let name = prompt('¿Cuál es el nombre “oficial” de JavaScript?');
-
-if(name == 'ECMAScript'){
-    alert('¡Correcto!');
-}
-else alert('¿No lo sabes? ¡ECMAScript!');
-
-
-let num = prompt('Ingrese un numero: ');
-if(num > 0){
-    alert('1');
-}
-else if(num < 0){
-    alert('-1');
-}
-else alert('0');
-*/
-
-// 15. Funciones 
-
-function showMessage(){
-    alert('mensaje desde una funcion');
+function nuevo(){                         // Crea una funcion llamada nuevo
+  let li = document.createElement('li');  // Crea un elemento li 
+  li.textContent = 'Nuevo elemento';      // Agrega texto dentro del li
+  ul.append(li);                        // Carga el contenido de li al final de ul
 }
 
-showMessage();
+let prueba = document.querySelector('#prueba');     // Obtiene el elemento con id prueba
+let prueba2 = document.querySelector('#prueba2');   // Obtiene el elemento con id prueba2
+let bt1 = document.querySelector('#bt1');           // Obtiene el elemento con id bt1
+let bt2 = document.querySelector('#bt2');           // Obtiene el elemento con id bt2
+
+function add(){                                     // Crea una funcion llamada add
+prueba.classList.add('resaltado');                  // Agrega
+if (prueba.classList.contains('resaltado')) {       // Condicion de si la clase es true
+  alert('El parrafo ya tiene la clase resaltado');  // Muestra un mensaje
+}
+}
+
+function toggle(){
+  prueba2.classList.toggle('resaltado');  
+}
+
+
