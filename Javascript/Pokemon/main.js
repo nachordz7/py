78631adcd68c4dc0e27d28e1d987713d7c65d0a9
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-08-11 21:52:41 -0300

    Actualizar main.js

--- Javascript/Pokemon/main.js
+++ Javascript/Pokemon/main.js
@@ -1,13 +1,16 @@
-let url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
-
-function pokemon(){
-let text = document.querySelector('#text');
-let img = document.querySelector('#imagen');
-
+function handleSubmit(event) {
+  event.preventDefault();
+  let pokemon = event.target.pokemon.value;
+  let url = 'https://pokeapi.co/api/v2/pokemon/';
+  let container = document.querySelector('.container');
+  container.innerHTML = '';
+  event.target.reset();
+  
+  fetch(url + pokemon)
+    .then(res => res.json()) 
+    .then(pokemon => {
+      let img = document.createElement('img');
+      img.src = pokemon.sprites.front_default;
+      container.append(img);
+    });
 }
-
-fetch(url)
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-});
