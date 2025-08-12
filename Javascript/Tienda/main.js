f6dfdb08c0ed680bdb72d34ba021c216dd1bd3fe
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-08-11 22:37:17 -0300

    Update main.js

--- Javascript/Tienda/main.js
+++ Javascript/Tienda/main.js
@@ -1,1 +1,12 @@
+function compra(event){
+    event.preventDefault();
+    let producto = event.target.producto.value;
+    let url = 'https://fakestoreapi.com/products/1';
+    let container = document.querySelector('.container');
+    container.innerHTML = '';
+    event.target.reset();
+}
 
+fetch(url + producto)
+.then(res=>res.json())            
+.then(json=>console.log(json))
