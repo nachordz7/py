6159ef2a246f651529f8e332ede520f69cdecd69
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-09-29 16:03:48 -0300

    Update script.js

--- Javascript/Repaso/script.js
+++ Javascript/Repaso/script.js
@@ -1,4 +1,3 @@
-
 function hora(){
     let reloj = document.querySelector('h2');
 
@@ -12,7 +11,29 @@ function hora(){
     s = s.toString().padStart(2,'0');
 
     reloj.textContent = `${h}:${m}:${s}`;
-    
 
 }
 setInterval(hora,1000);
+
+//Usar una API
+let container = document.querySelector('.container');
+let url = 'https://restcountries.com/v3.1/all?fields=flag,name,capital';
+
+function createCountryCard(country) {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    let h3 = document.createElement('h3');
+    let h4 = document.createElement('h4');
+    h3.textContent = country.name.common;
+    h4.textContent = country.capital;
+    card.append(h3,h4);
+    container.append(card);
+  }
+
+fetch(url)
+  .then(res => res.json())
+  .then(countries => {
+    for (let country of countries)
+      createCountryCard(country);
+  })
+  .catch(err => console.error(err));
