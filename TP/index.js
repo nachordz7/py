2aa791d441d05e56bbeb4ecbf2837d9726093acd
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-10-16 21:14:23 -0300

    Actualizar index.js

--- TP/index.js
+++ TP/index.js
@@ -66,3 +66,63 @@ app.get('/api/test', (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+
+
+
+
+/*
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+app.use(cors()) ;
+app.use(express.json());
+app.use(morgan('dev')) ;
+
+
+const conn = mysql.createConnection({
+  host: 'localhost', 
+  database: 'tp'
+});
+
+app.post('/api/asistencias', (req, res) => {
+      const { tipo, alumno, materia } = req.body;
+      const data = [tipo, alumno, materia];
+      const q = 'INSERT INTO registros(tipo, alumno, materia) VALUES(?,?,?)';
+ conn.query(q, data, (err, rs) => {
+   res.status(201).json({ msg: 'Alta OK'});
+ }) ;
+}) ;
+
+//dame todos los cursos
+app.get('/api/cursos', (req, res) => {
+  conn.query('SELECT * FROM cursos', (err, rs) =>
+  {res.status(200).json(rs);
+  });
+});
+
+//dame todos los alumnos de materias
+app.get('/api/alumnos/:materia',(req, res) =>{
+  const materia = req.params.curso;
+  const q = 'SELECT a.id, a.nombres, a.apellidos FROM alumnos a JOIN cursos c ON a.curso = c.id JOIN materias m ON m.curso = c.id WHERE m.id =?';
+  conn.query(q, [materia], (err, rs) => {
+    res.status(200).json(rs);
+  });
+});
+
+
+//dame todas las materias de un curso
+app.get('/api/materias/:curso', (req, res) =>{
+  const curso = req.params.curso;
+  conn.query('SELECT * FROM materias WHERE curso = ?', [curso], (err, rs) => {
+  res.status(200).json(rs);
+});
+});
+conn.connect(() => console.log('base de datos conectada'));
+
+app.listen(3000, () => console.log('server online en localhost:3000'));
+
+
+
+
+/*
